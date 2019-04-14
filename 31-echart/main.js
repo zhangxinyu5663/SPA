fc71bd1da82f1d5e174cfacc07bb43c6e549261a
zhangxinyu5663 <1976871412@qq.com>
2019-04-14 17:04:20 +0800

    echart图表

---
+++ 31-echart/main.js
@@ -0,0 +1,40 @@
+/* global echarts: true */
+$(function(){
+  //var myChart = echarts.init(document.getElementById('main'));
+  var myChart=echarts.init($('.main')[0]);
+  var xData=[],
+      yData=[];
+  //create xData and yData
+  for(var p=0;p<=1;p+=0.1){
+    xData.push(p);
+    yData.push(h(p));
+    //console.log(`p=${p},h(p)=${h(p)}`)
+  }
+  function h(p){
+    if(p==0){
+      return 0;
+    }
+    return -1*p*Math.log2(p)-(1-p)*Math.log2(1-p);
+  }
+
+  var option = {
+    title: {
+      text: '二进熵函数曲线'         
+    },
+    tooltip: {},
+    legend: {
+      data:['二进熵']             
+    },
+    xAxis: {
+      data:xData 
+    },
+    yAxis: {},
+    series: [{
+      name: '二进熵',
+      type: 'line', //折线状
+      smooth:true,
+      data: yData
+    }]      
+  };
+  myChart.setOption(option);
+});
