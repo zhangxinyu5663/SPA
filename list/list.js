b75ab06db1bb42f3d8f32e74f7ea26c16b9f318a
zhangxinyu5663 <1976871412@qq.com>
2019-04-17 09:30:50 +0800

    列表框组件

--- list/list.js
+++ list/list.js
@@ -1,114 +1,115 @@
 /* exported comList */
 function comList() {
-  var $comList = $(''
+    var $comList = $(''
         + '<div class="notepad-com-list">'
           + '<input class="editor" type="text"><br>'
           + '<ul class="list">'
           + '</ul>'
         + '</div>');
   
-  var $editor = $comList.find('.editor'),
-      $list = $comList.find('.list'),
-      $items;
+    var $editor = $comList.find('.editor'),
+        $list = $comList.find('.list'),
+        $items;
   
-  var cfg = {
-    container: '',
-    list: [],
-    select: 0,
-    width: '200px',
-    isFont: false,
-    isFontStyle: false,
-    selectHandler: null
-  };
+    var cfg = {
+      container: '',
+      list: [],
+      select: 0,
+      width: '200px',
+      isFont: false,
+      isFontStyle: false,
+      selectHandler: null
+    };
   
-  function setFontStyle(item, style) {
-    if(style === '斜体') {
-      item.css({'font-style': 'italic'});
-      return;
-    }
-  
-    if(style === '粗体') {
-      item.css({'font-weight': 'bold'});
-      return;
-    }
-  
-    if(style === '粗偏斜体') {
-      item.css({'font-weight': 'bold', 'font-style': 'italic'});
-      return;
-    }
-  }
-  
-  function fillData() {
-    var i = 0, $item;
-  
-    if(cfg.isFont) {
-      for(i=0; i<cfg.list.length; i++) {
-        $item = $('<li class="item"></li>');
-        $item.css({'font-family': cfg.list[i]});
-        $list.append($item.html(cfg.list[i]));
+    function setFontStyle(item, style) {
+      if(style === '斜体') {
+        item.css({'font-style': 'italic'});
+        return;
       }
-    } else if(cfg.isFontStyle) {
-      for(i=0; i<cfg.list.length; i++) {
-        $item = $('<li class="item"></li>');
-        setFontStyle($item, cfg.list[i]);
-        $list.append($item.html(cfg.list[i]));
+  
+      if(style === '粗体') {
+        item.css({'font-weight': 'bold'});
+        return;
       }
-    } else {
-      for(i=0; i<cfg.list.length; i++) {
-        $item = $('<li class="item"></li>');
-        $list.append($item.html(cfg.list[i]));
+  
+      if(style === '粗偏斜体') {
+        item.css({'font-weight': 'bold', 'font-style': 'italic'});
+        return;
       }
     }
   
-    $items = $list.find('.item');
-  }
+    function fillData() {
+      var i = 0, $item;
   
-  function setSelect(n) {
-    $($items[n]).addClass('selected');
-    $editor.val(cfg.list[n]);
-    $editor.select();
-  }
+      if(cfg.isFont) {
+        for(i=0; i<cfg.list.length; i++) {
+          $item = $('<li class="item"></li>');
+          $item.css({'font-family': cfg.list[i]});
+          $list.append($item.html(cfg.list[i]));
+        }
+      } else if(cfg.isFontStyle) {
+        for(i=0; i<cfg.list.length; i++) {
+          $item = $('<li class="item"></li>');
+          setFontStyle($item, cfg.list[i]);
+          $list.append($item.html(cfg.list[i]));
+        }
+      } else {
+        for(i=0; i<cfg.list.length; i++) {
+          $item = $('<li class="item"></li>');
+          $list.append($item.html(cfg.list[i]));
+        }
+      }
   
-  function init() {
-    $comList.css({width: cfg.width});
-    fillData();
-    setSelect(cfg.select);
-    //$(cfg.container).append($comList);
-    $comList.appendTo($(cfg.container));
-  }
+      $items = $list.find('.item');
+    }
   
-  this.show = function(conf) {
-    $.extend(cfg, conf);
-    //init();
-    $comList.css({width: cfg.width});
-    fillData();
-    setSelect(cfg.select);
-    $(cfg.container).append($comList);
-    //$comList.appendTo($(cfg.container));
-    console.log($(cfg.container).html());
-
-    $list.click(function(e) {
-      $($items[cfg.select]).removeClass('selected');
-      cfg.select = cfg.list.indexOf($(e.target).html());
-      $($items[cfg.select]).addClass('selected');
-      $editor.val(cfg.list[cfg.select]);
+    function setSelect(n) {
+      $($items[n]).addClass('selected');
+      $editor.val(cfg.list[n]);
       $editor.select();
-      cfg.selectHandler(cfg.select);
-    });
+    }
   
-    $editor.keyup(function() {
-      var i = 0;
+    function init() {
+      $comList.css({width: cfg.width});
+      fillData();
+      setSelect(cfg.select);
+      //$(cfg.container).append($comList);
+      $comList.appendTo($(cfg.container));
+    }
   
-      for(i=0; i<cfg.list.length; i++) {
-        if(cfg.list[i].indexOf($editor.val()) === 0) break;
-      }
+    this.show = function(conf) {
+      $.extend(cfg, conf);
+      //init();
+       $comList.css({width: cfg.width});
+      fillData();
+      setSelect(cfg.select);
+      $(cfg.container).append($comList);
+      //$comList.appendTo($(cfg.container));
+      console.log($(cfg.container).html());
+
   
-      if(i === cfg.list.length) return;
+      $list.click(function(e) {
+        $($items[cfg.select]).removeClass('selected');
+        cfg.select = cfg.list.indexOf($(e.target).html());
+        $($items[cfg.select]).addClass('selected');
+        $editor.val(cfg.list[cfg.select]);
+        $editor.select();
+        cfg.selectHandler(cfg.select);
+      });
   
-      $items[i].scrollIntoView({behavior: 'smooth', block: 'start'});
-      $($items[cfg.select]).removeClass('selected');
-      $($items[i]).addClass('selected');
-      cfg.select = i;
-    });
-  };
-}
+      $editor.keyup(function() {
+        var i = 0;
+  
+        for(i=0; i<cfg.list.length; i++) {
+          if(cfg.list[i].indexOf($editor.val()) === 0) break;
+        }
+  
+        if(i === cfg.list.length) return;
+  
+        $items[i].scrollIntoView({behavior: 'smooth', block: 'start'});
+        $($items[cfg.select]).removeClass('selected');
+        $($items[i]).addClass('selected');
+        cfg.select = i;
+      });
+    };
+  }
